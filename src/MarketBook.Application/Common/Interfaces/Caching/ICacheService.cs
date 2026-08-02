5eff9264b1a48c98fb8e86b2f829726152b230e7
Saeidasadi32 <asadi_s@yahoo.com>
2026-08-02 07:28:20 +0330

    feat(application): complete application foundation

---
+++ src/MarketBook.Application/Common/Interfaces/Caching/ICacheService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarketBook.Application.Common.Interfaces.Caching
+{
+    internal class ICacheService
+    {
+    }
+}
---
+++ src/MarketBook.Application/Common/Interfaces/Clock/IDateTimeProvider.cs
@@ -0,0 +1,26 @@
+﻿// -----------------------------------------------------------------------------
+// Project   : MarketBook (Intelligent Market Book System)
+// Platform  : MarketBook Platform
+// Layer     : Application
+// Namespace : MarketBook.Application.Common.Interfaces.Clock
+//
+// Copyright (c) Saeid Asadi. All rights reserved.
+// Licensed under the MIT License.
+// -----------------------------------------------------------------------------
+
+namespace MarketBook.Application.Common.Interfaces.Clock;
+
+/// <summary>
+/// EN: Provides the current date and time.
+///
+/// FA: تاریخ و زمان جاری را فراهم می‌کند.
+/// </summary>
+public interface IDateTimeProvider
+{
+    /// <summary>
+    /// EN: Gets current UTC date and time.
+    ///
+    /// FA: تاریخ و زمان فعلی UTC را دریافت می‌کند.
+    /// </summary>
+    DateTimeOffset UtcNow { get; }
+}

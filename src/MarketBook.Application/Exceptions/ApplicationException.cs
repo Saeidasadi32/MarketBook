4e9bccfec373c158adccb659eaf24ea0e08fdada
Saeidasadi32 <asadi_s@yahoo.com>
2026-08-03 07:54:36 +0330

    chore: finalize application foundation

---
+++ src/MarketBook.Application/Exceptions/ApplicationException.cs
@@ -0,0 +1,60 @@
+﻿// -----------------------------------------------------------------------------
+// Project   : MarketBook (Intelligent Market Book System)
+// Platform  : MarketBook Platform
+// Layer     : Application
+// Namespace : MarketBook.Application.Exceptions
+//
+// Copyright (c) Saeid Asadi. All rights reserved.
+// Licensed under the MIT License.
+// -----------------------------------------------------------------------------
+
+using MarketBook.Domain.Common;
+
+namespace MarketBook.Application.Exceptions;
+
+/// <summary>
+/// EN: Represents the base exception for the Application layer.
+/// FA: استثنای پایه برای لایه Application را نمایش می‌دهد.
+/// </summary>
+public class ApplicationException : Exception
+{
+    /// <summary>
+    /// EN: Initializes a new instance of the <see cref="ApplicationException"/> class.
+    /// FA: نمونه جدیدی از کلاس <see cref="ApplicationException"/> را ایجاد می‌کند.
+    /// </summary>
+    /// <param name="error">
+    /// EN: Application error.
+    /// FA: خطای برنامه.
+    /// </param>
+    public ApplicationException(Error error)
+        : base(error.Message)
+    {
+        Error = error;
+    }
+
+    /// <summary>
+    /// EN: Initializes a new instance of the <see cref="ApplicationException"/> class.
+    /// FA: نمونه جدیدی از کلاس <see cref="ApplicationException"/> را ایجاد می‌کند.
+    /// </summary>
+    /// <param name="error">
+    /// EN: Application error.
+    /// FA: خطای برنامه.
+    /// </param>
+    /// <param name="innerException">
+    /// EN: Inner exception.
+    /// FA: استثنای داخلی.
+    /// </param>
+    public ApplicationException(
+        Error error,
+        Exception innerException)
+        : base(error.Message, innerException)
+    {
+        Error = error;
+    }
+
+    /// <summary>
+    /// EN: Gets the associated error.
+    /// FA: خطای مرتبط را دریافت می‌کند.
+    /// </summary>
+    public Error Error { get; }
+}
--- src/MarketBook.Application/Exceptions/ValidationException.cs
+++ src/MarketBook.Application/Exceptions/ValidationException.cs
@@ -1,10 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// -----------------------------------------------------------------------------
+// Project   : MarketBook (Intelligent Market Book System)
+// Platform  : MarketBook Platform
+// Layer     : Application
+// Namespace : MarketBook.Application.Exceptions
+//
+// Copyright (c) Saeid Asadi. All rights reserved.
+// Licensed under the MIT License.
+// -----------------------------------------------------------------------------
 
-namespace MarketBook.Application.Exceptions
+using FluentValidation.Results;
+
+namespace MarketBook.Application.Exceptions;
+
+/// <summary>
+/// EN: Represents a validation exception.
+/// FA: استثنای اعتبارسنجی را نمایش می‌دهد.
+/// </summary>
+public sealed class ApplicationValidationException : Exception
 {
-    internal class ValidationException
+    /// <summary>
+    /// EN: Initializes a new instance of the <see cref="ApplicationValidationException"/> class.
+    /// FA: نمونه جدیدی از کلاس <see cref="ApplicationValidationException"/> را ایجاد می‌کند.
+    /// </summary>
+    /// <param name="failures">
+    /// EN: Validation failures.
+    /// FA: خطاهای اعتبارسنجی.
+    /// </param>
+    public ApplicationValidationException(
+        IEnumerable<ValidationFailure> failures)
+        : base("One or more validation failures have occurred.")
     {
+        Errors = failures
+            .GroupBy(x => x.PropertyName)
+            .ToDictionary(
+                g => g.Key,
+                g => g.Select(x => x.ErrorMessage).ToArray());
     }
+
+    /// <summary>
+    /// EN: Gets validation errors.
+    /// FA: خطاهای اعتبارسنجی را دریافت می‌کند.
+    /// </summary>
+    public IReadOnlyDictionary<string, string[]> Errors { get; }
 }
